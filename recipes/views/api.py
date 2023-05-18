76de37edbfb9f531c5852063a6d44c011cd046e8
Leonardo Melo <leonardo.mel04@outlook.com>
2023-05-18 12:54:17 -0300

    Save author when creating recipe

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -27,6 +27,15 @@ class RecipeAPIv2ViewSet(ModelViewSet):
         IsOwnerOrReadOnly,
     ]
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
 
 # TAGS
 @api_view()
